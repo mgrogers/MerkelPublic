f21eeec904b9af8e63078b149c533af2101a5021
Tim Shi <timgshi@gmail.com>
2013-02-09 13:10:53 -0800

    created xcode project

---
+++ Xcode/Merkel/MerkelTests/MerkelTests.m
@@ -0,0 +1,32 @@
+//
+//  MerkelTests.m
+//  MerkelTests
+//
+//  Created by Tim Shi on 2/9/13.
+//  Copyright (c) 2013 BossMobileWunderkinds. All rights reserved.
+//
+
+#import "MerkelTests.h"
+
+@implementation MerkelTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in MerkelTests");
+}
+
+@end
