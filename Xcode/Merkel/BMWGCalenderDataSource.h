05004bdd67ee09ec4425f080f09ea07ce243e363
Tim Shi <timgshi@gmail.com>
2013-02-25 13:57:50 -0800

    Starting shared data source implementation.

---
+++ Xcode/Merkel/BMWGCalenderDataSource.h
@@ -0,0 +1,15 @@
+//
+//  BMWGCalenderDataSource.h
+//  Merkel
+//
+//  Created by Tim Shi on 2/25/13.
+//  Copyright (c) 2013 BossMobileWunderkinds. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BMWGCalenderDataSource : NSObject
+
++ (instancetype)sharedDataSource;
+
+@end
