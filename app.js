3c61c931700b1a78a06d89866f3478a60a20eeba
Tony Jin <imtonyjin@gmail.com>
2013-02-25 14:22:36 -0800

    Refactoring calendar test into main app

--- app.js
+++ app.js
@@ -34,6 +34,7 @@ app.configure('development', function(){
 //Define API URLS and destinations here.
 app.get('/', routes.index);
 app.get('/calendar', calendar.list);
+app.get('/authentication', calendar.authentication);
 app.get('/twilio', twilio.twilio);
 
 http.createServer(app).listen(app.get('port'), function(){
