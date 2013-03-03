179832494bb14a26cb88dba6bb52074917741a74
Wesley Leung <Wes.k.Leung@gmail.com>
2013-03-02 21:06:14 -0800

    added BMWLinkedInView for testing

---
+++ Xcode/BMWLinkedInView.m
@@ -0,0 +1,16 @@
+//
+//  BMWLinkedInView.m
+//  Merkel
+//
+//  Created by Wesley Leung on 3/2/13.
+//  Copyright (c) 2013 BossMobileWunderkinds. All rights reserved.
+//
+
+#import "BMWLinkedInView.h"
+
+@implementation BMWLinkedInView
+
+
+
+
+@end
