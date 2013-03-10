1a82a5323bbe7464f37abbf62bb9de145fefb0e5
Tim Shi <timgshi@gmail.com>
2013-03-10 15:16:38 -0700

    got initial detail screen working

--- Xcode/Merkel/BMWCalendarEventView.m
+++ Xcode/Merkel/BMWCalendarEventView.m
@@ -14,6 +14,8 @@
 @interface BMWCalendarEventView ()
 
 @property (nonatomic, strong) IDLabel *titleLabel, *descriptionLabel, *startLabel, *endLabel;
+@property (nonatomic, strong) NSArray *allViews, *detailViews, *placeViews, *peopleViews;
+@property (nonatomic, strong) NSMutableArray *allWidgets;
 
 @end
 
@@ -29,14 +31,17 @@
 //    self.endLabel = [IDLabel label];
 //    self.widgets = @[self.titleLabel, self.descriptionLabel, self.startLabel, self.endLabel];
     self.toolbarWidgets = [self createToolbarButtons];
+    [self createAllViews];
+    self.widgets = self.allWidgets;
 }
 
 - (void)viewDidBecomeFocused:(IDView *)view {
-    self.event = [self.eventDelegate eventForEventView:self];
+//    self.event = [self.eventDelegate eventForEventView:self];
+    self.event = [BMWGCalendarEvent testEvent];
     if (self.event) {
         self.title = self.event.title;
-//        self.titleLabel.text = self.event.title;
-//        self.descriptionLabel.text = self.event.eventDescription;
+        self.titleLabel.text = self.event.title;
+        self.descriptionLabel.text = self.event.eventDescription;
 //        NSDateFormatter *formatter = [[NSDateFormatter alloc] init];
 //        [formatter setDateStyle:NSDateFormatterMediumStyle];
 //        [formatter setTimeStyle:NSDateFormatterMediumStyle];
@@ -54,6 +59,7 @@
         if (data) {
             toolbarButton.imageData = data;
         }
+        toolbarButton.text = [self tooltipForIndex:i];
         [toolbarButton setTarget:self selector:@selector(toolbarButtonPressed:) forActionEvent:IDActionEventSelect];
         [toolbarButton setTarget:self selector:@selector(toolbarButtonFocused:) forActionEvent:IDActionEventFocus];
         [buttons addObject:toolbarButton];
@@ -61,7 +67,9 @@
     return buttons;
 }
 
-- (IDImageData *)imageForIndex:(NSInteger)i
+#pragma mark - Toolbar Setup
+
+- (IDImageData *)imageForIndex:(NSInteger)index
 {
     static NSArray *images = nil;
     static dispatch_once_t onceToken;
@@ -71,17 +79,78 @@
                    [self.application.imageBundle imageWithName:@"iconMan" type:@"png"]];
 
     });
-    return [images objectAtIndex:i];
+    return [images objectAtIndex:index];
+}
+
+- (NSString *)tooltipForIndex:(NSInteger)index {
+    static NSArray *tooltips = nil;
+    static dispatch_once_t onceToken;
+    dispatch_once(&onceToken, ^{
+        tooltips = @[@"Details",
+                     @"Place",
+                     @"People"];
+    });
+    return [tooltips objectAtIndex:index];
+}
+
+#pragma mark - Screen Setup
+
+- (NSArray *)createAllViews {
+    self.allWidgets = [NSMutableArray array];
+    self.allViews = @[[self createDetailViews], [self createPlaceViews], [self createPeopleViews]];
+    return self.allViews;
+}
+
+- (NSArray *)createDetailViews {
+    self.titleLabel = [IDLabel label];
+    self.titleLabel.selectable = NO;
+//    self.titleLabel.position = CGPointMake(0, 8);
+    self.titleLabel.isInfoLabel = YES;
+    self.titleLabel.visible = NO;
+    self.descriptionLabel = [IDLabel label];
+    self.descriptionLabel.selectable = NO;
+//    self.descriptionLabel.position = CGPointMake(0, 20);
+    self.descriptionLabel.visible = NO;
+    self.detailViews = @[self.titleLabel, self.descriptionLabel];
+    [self.allWidgets addObjectsFromArray:self.detailViews];
+    return self.detailViews;
+}
+
+- (NSArray *)createPlaceViews {
+    self.placeViews = @[];
+    [self.allWidgets addObjectsFromArray:self.placeViews];
+    return self.placeViews;
+}
+
+- (NSArray *)createPeopleViews {
+    self.peopleViews = @[];
+    [self.allWidgets addObjectsFromArray:self.peopleViews];
+    return self.peopleViews;
 }
 
 #pragma mark - Event handlers
 
 - (void)toolbarButtonPressed:(IDButton *)button
 {
+//    NSInteger index = [self.toolbarWidgets indexOfObject:button];
 }
 
 - (void)toolbarButtonFocused:(IDButton *)button
 {
+    NSInteger index = [self.toolbarWidgets indexOfObject:button];
+    [self updateDisplayForIndex:index];
+}
+
+- (void)updateDisplayForIndex:(NSInteger)index {
+    for (NSArray *view in self.allViews) {
+        for (IDWidget *widget in view) {
+            widget.visible = NO;
+        }
+    }
+    NSArray *curView = self.allViews[index];
+    for (IDWidget *widget in curView) {
+        widget.visible = YES;
+    }
 }
 
 @end
