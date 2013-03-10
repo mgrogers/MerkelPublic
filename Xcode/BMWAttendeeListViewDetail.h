97ac22193a1c7606c38e87c5aa479f813d8c9297
Wesley Leung <Wes.k.Leung@gmail.com>
2013-03-10 14:09:43 -0700

    added attendee list views

---
+++ Xcode/BMWAttendeeListViewDetail.h
@@ -0,0 +1,13 @@
+//
+//  BMWAttendeeListViewDetail.h
+//  Merkel
+//
+//  Created by Wesley Leung on 3/10/13.
+//  Copyright (c) 2013 BossMobileWunderkinds. All rights reserved.
+//
+
+#import <BMWAppKit/BMWAppKit.h>
+
+@interface BMWAttendeeListViewDetail : IDView
+
+@end
