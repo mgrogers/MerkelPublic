6df8bd1d2a7272f4ac28892a459c54c357ef237f
Tim Shi <timgshi@gmail.com>
2013-03-12 19:33:35 -0700

    got sms api working

--- consumer.js
+++ consumer.js
@@ -2,16 +2,16 @@ var kue = require('kue')
   , url = require('url')
   , redis = require('kue/node_modules/redis');
 
-var parse = require('node-parse-api').Parse;
+var Parse = require('parse').Parse;
 var PARSE_APP_ID = "ljgVpGcSO3tJlAFRosuoGhLuWElPbWapt4Wy5uoj";
-var PARSE_MASTER_KEY = "AAOYtk81wI3iRJiXxgRfwblt1EUHVBlyvpS9m3QO";
-var parseApp = new parse(PARSE_APP_ID, PARSE_MASTER_KEY);
+var PARSE_JAVASCRIPT_KEY = "LuF9wF7h9GRzarp6FnYjdGUGHQtOrlxUeoC3CJqM";
+Parse.initialize(PARSE_APP_ID, PARSE_JAVASCRIPT_KEY);
  
 // make sure we use the Heroku Redis To Go URL
 // (put REDISTOGO_URL=redis://localhost:6379 in .env for local testing)
  
 kue.redis.createClient = function() {
-    var redisUrl = url.parse(process.env.REDISTOGO_URL)
+    var redisUrl = url.parse(process.env.REDISTOGO_URL || "redis://localhost:6379")
       , client = redis.createClient(redisUrl.port, redisUrl.hostname);
     if (redisUrl.auth) {
         client.auth(redisUrl.auth.split(":")[1]);
@@ -20,6 +20,8 @@ kue.redis.createClient = function() {
 };
  
 var jobs = kue.createQueue();
+
+var delayCheck = 1000;
  
 // see https://github.com/learnBoost/kue/ for how to do more than one job at a time
 jobs.process('crawl', function(job, done) {
@@ -28,17 +30,20 @@ jobs.process('crawl', function(job, done) {
 });
 
 jobs.process('sms', 20, function(job, done) {
+  console.log(job.data);
   var data = {"to": job.data.to,
-              "from": job.data.from,
               "body": job.data.body};
-  Parse.Cloud.run('send-sms', data, {
+  Parse.Cloud.run('sendsms', data, {
     success: function(response) {
-      console.log("success");
+      job.log("success");
       console.log(response);
       done();
     },
     error: function(error) {
+      job.log(error);
       done(error);
     }
   });
 });
+
+jobs.promote(delayCheck);
--- routes/sms.js
+++ routes/sms.js
@@ -1,12 +1,105 @@
 var kue = require('kue')
  , jobs = kue.createQueue();
 
-exports.sendsms = function (data, delay) {
-	var timedelay = (delay > 0) ? delay : 0;
-	var job = jobs.create('sms', data).delay(timedelay).save();
-	job.on('complete', function(){
-	  console.log("Job complete: " + job);
-	}).on('failed', function(){
-	  console.log("Job failed: " + job);
-	})
+exports.sendsms = function (req, res) {
+    var data = req.query;
+    data.title = "SMS to: " + data.to;
+    console.log(data);
+    var timedelay = parseInt(data.delay);
+    timedelay = (timedelay > 0) ? timedelay : 0;
+    console.log(timedelay);
+    var job = jobs.create('sms', data);
+    if (timedelay > 0) {
+        job.delay(timedelay);
+        if (req.query.to != null && req.query.body != null) {
+            var response = {"meta": {
+                                "code": 200
+                                },
+                            "data": data,
+                            "message": "SMS scheduled " + timedelay + " ms from " + new Date()
+                            };
+            res.send(response);
+        } else {
+            var response = {"meta": {
+                                "code": 400,
+                                "error_message": "Insufficient parameters for delayed SMS."
+                                },
+                            "data": data
+                            };
+            res.send(400, response);
+        }
+    } else {
+      console.log("less than 0");
+        job.on('complete', function(){
+          console.log("Job complete: " + job);
+          var response = {"meta": {
+                            "code": 200
+                            },
+                        "data": data,
+                        "message": "SMS delivered"
+                        };
+          res.send(response);
+        }).on('failed', function(){
+          console.log("Job failed: " + job);
+          var response = {"meta": {
+                            "code": 400,
+                            "error_message": "Insufficient parameters for SMS."
+                            },
+                        "data": data
+                        };
+          res.send(400, data);
+        });
+    }
+    job.save();
+}
+
+exports.sendsms_helper = function (data) {
+    data.title = "SMS to: " + data.to;
+    console.log(data);
+    var timedelay = parseInt(data.delay);
+    timedelay = (timedelay > 0) ? timedelay : 0;
+    console.log(timedelay);
+    var job = jobs.create('sms', data);
+    if (timedelay > 0) {
+        job.delay(timedelay);
+        if (req.query.to != null && req.query.body != null) {
+            var response = {"meta": {
+                                "code": 200
+                                },
+                            "data": data,
+                            "message": "SMS scheduled " + timedelay + " ms from " + new Date()
+                            };
+            res.send(response);
+        } else {
+            var response = {"meta": {
+                                "code": 400,
+                                "error_message": "Insufficient parameters for delayed SMS."
+                                },
+                            "data": data
+                            };
+            res.send(400, response);
+        }
+    } else {
+      console.log("less than 0");
+        job.on('complete', function(){
+          console.log("Job complete: " + job);
+          var response = {"meta": {
+                            "code": 200
+                            },
+                        "data": data,
+                        "message": "SMS delivered"
+                        };
+          res.send(response);
+        }).on('failed', function(){
+          console.log("Job failed: " + job);
+          var response = {"meta": {
+                            "code": 400,
+                            "error_message": "Insufficient parameters for SMS."
+                            },
+                        "data": data
+                        };
+          res.send(400, data);
+        });
+    }
+    job.save();
 }
