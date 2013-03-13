c53d9022fe5012d5b8a296a403f1f8947a41712e
Tim Shi <timgshi@gmail.com>
2013-03-12 19:35:29 -0700

    wrote helper function for calling sms without http

--- routes/sms.js
+++ routes/sms.js
@@ -62,44 +62,11 @@ exports.sendsms_helper = function (data) {
     var job = jobs.create('sms', data);
     if (timedelay > 0) {
         job.delay(timedelay);
-        if (req.query.to != null && req.query.body != null) {
-            var response = {"meta": {
-                                "code": 200
-                                },
-                            "data": data,
-                            "message": "SMS scheduled " + timedelay + " ms from " + new Date()
-                            };
-            res.send(response);
-        } else {
-            var response = {"meta": {
-                                "code": 400,
-                                "error_message": "Insufficient parameters for delayed SMS."
-                                },
-                            "data": data
-                            };
-            res.send(400, response);
-        }
-    } else {
-      console.log("less than 0");
-        job.on('complete', function(){
-          console.log("Job complete: " + job);
-          var response = {"meta": {
-                            "code": 200
-                            },
-                        "data": data,
-                        "message": "SMS delivered"
-                        };
-          res.send(response);
-        }).on('failed', function(){
-          console.log("Job failed: " + job);
-          var response = {"meta": {
-                            "code": 400,
-                            "error_message": "Insufficient parameters for SMS."
-                            },
-                        "data": data
-                        };
-          res.send(400, data);
-        });
     }
+    job.on('complete', function(){
+      console.log("Job complete: " + job);
+    }).on('failed', function(){
+      console.log("Job failed: " + job);
+    });
     job.save();
 }
