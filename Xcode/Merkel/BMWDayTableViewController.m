940510c8f8c2db922c17b70540a813dbd500c704
Tim Shi <timgshi@gmail.com>
2013-04-16 02:16:24 -0700

    started mock tableview implementation for cell ui customization

---
+++ Xcode/Merkel/BMWDayTableViewController.m
@@ -0,0 +1,86 @@
+//
+//  BMWDayTableViewController.m
+//  Merkel
+//
+//  Created by Tim Shi on 4/16/13.
+//  Copyright (c) 2013 BossMobileWunderkinds. All rights reserved.
+//
+
+#import "BMWDayTableViewController.h"
+
+@interface BMWDayTableViewController ()
+
+@property (nonatomic, strong) NSArray *testData;
+
+@end
+
+@implementation BMWDayTableViewController
+
+- (id)initWithStyle:(UITableViewStyle)style {
+    self = [super initWithStyle:style];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (NSArray *)testData {
+    if (!_testData) {
+        _testData = @[@{@"title": @"Daily Scrum",
+                        @"start": @"10:30am",
+                        @"end": @"11:30am"},
+                      @{@"title": @"Lunch Break",
+                        @"start": @"11:45am",
+                        @"end": @"12:30pm"},];
+    }
+    return _testData;
+}
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    [self.tableView registerClass:[UITableViewCell class] forCellReuseIdentifier:@"Cell"];
+    self.title = @"My Day";
+    UIButton *button = [UIButton buttonWithType:UIButtonTypeCustom];
+    button.frame = CGRectMake(0.0, 0.0, 25.0, 19.0);
+    button.backgroundColor = [UIColor clearColor];
+    [button setBackgroundImage:[UIImage imageNamed:@"reveal_menu_icon_portrait.png"] forState:UIControlStateNormal];
+    UIBarButtonItem *menuBarButton = [[UIBarButtonItem alloc] initWithCustomView:button];
+    UIBarButtonItem *spacer = [[UIBarButtonItem alloc] initWithBarButtonSystemItem:UIBarButtonSystemItemFixedSpace target:nil action:nil];
+    spacer.width = 10.0;
+    self.navigationItem.leftBarButtonItems = @[spacer, menuBarButton];
+}
+
+- (void)viewDidAppear:(BOOL)animated {
+    [super viewDidAppear:animated];
+    [self.tableView reloadData];
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+#pragma mark - Table view data source
+
+- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+    return 1;
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.testData.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+    static NSString *CellIdentifier = @"Cell";
+    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:CellIdentifier forIndexPath:indexPath];
+    NSDictionary *item = self.testData[indexPath.row];
+    cell.textLabel.text = item[@"title"];
+    return cell;
+}
+
+#pragma mark - Table view delegate
+
+- (void)tableView:(UITableView *)tableView didSelectRowAtIndexPath:(NSIndexPath *)indexPath {
+}
+
+@end
