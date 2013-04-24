d768c030b7eade2b1dfba17f95739bd88beb3754
Wesley Leung <Wes.k.Leung@gmail.com>
2013-04-23 22:38:24 -0700

    added addressbook framework

---
+++ Xcode/Merkel/BMWAddressBookViewController.h
@@ -0,0 +1,14 @@
+//
+//  BMWAddressBookViewController.h
+//  Merkel
+//
+//  Created by Wesley Leung on 4/23/13.
+//  Copyright (c) 2013 BossMobileWunderkinds. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <AddressBook/AddressBook.h>
+#import <AddressBookUI/AddressBookUI.h>
+
+@interface BMWAddressBookViewController : UIViewController <ABPeoplePickerNavigationControllerDelegate> 
+@end
---
+++ Xcode/Merkel/BMWAddressBookViewController.m
@@ -0,0 +1,38 @@
+//
+//  BMWAddressBookViewController.m
+//  Merkel
+//
+//  Created by Wesley Leung on 4/23/13.
+//  Copyright (c) 2013 BossMobileWunderkinds. All rights reserved.
+//
+
+#import "BMWAddressBookViewController.h"
+
+@interface BMWAddressBookViewController ()
+
+@end
+
+@implementation BMWAddressBookViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
