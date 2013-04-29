8c830f321be6f6d9253f11e12f68416f7011b815
mgrogers <mjgrogers@gmail.com>
2013-04-29 02:10:39 -0700

    Email alerts (both invite and "running late") are working via API + dummy data

--- routes/sendgrid.js
+++ routes/sendgrid.js
@@ -1,6 +1,6 @@
 /* --------- API Function ------------ */
 /*API call: /api/sendgrid/alert
-[type] Message type - single word "invite" or "alert"
+[msgType] Message type - single word "invite" or "alert"
 [initiator] Person initiating the conference call or an alert that he or she is running late 
 [attendees] Unique identifier for a conference call 
 */
@@ -21,21 +21,32 @@ exports.emailAlert = function(req, res) {
 		key = process.env.SENDGRID_PASSWORD;
 	}
 	var sendgrid = new SendGrid(user, key);
-	var data = req.query;
-  var messageType = data.type;
+	var data = req.body;
+  var messageType = data.msgType;
   var initiator = data.initiator;
   var conferenceAttendees = data.attendees;
+  var sender, msgSubject, body;
 
   var Email = require('sendgrid').Email;
   var email = new Email(optionalParams);
   var optionalParams = {
     html: ''
   };
+
+  if(messageType == 'invite') {
+    sender = 'Invite@CallInapp.com';
+    msgSubject = initiator + " has invited you to a conference call with CallIn!";
+    body = "You're receiving this email because " + initiator + " has initiated a conference call with CallIn, and would like you to join!";
+  } else {
+    sender = 'Alert@CallInapp.com';
+    msgSubject = initiator + " has sent you an alert regarding your upcoming conference call";
+    body = "Sometimes life throws you curveballs, and it's how you respond that defines you. That's why you're receiving this email: to let you know that " + initiator + " is running lateand will be joining the conference call as soon as possible.";
+  }
   var email = new Email({
-    to: ["mjgrogers@gmail.com"],
-    from: 'Invite@CallInapp.com',
-    subject: 'Mike has invited you to a conference call via CallIn!',
-    text: "Bitch bitch, mccbitch"
+    to: conferenceAttendees, //e.g. ["mjgrogers@gmail.com", "bossmobilewunderkinds@lists.stanford.edu", .....]
+    from: sender,
+    subject: msgSubject,
+    text: body
   });
   sendgrid.send(email, function(success, message) {
   	  if(!success) {
