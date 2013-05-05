46da90a8733ae9605c6fa0f72e4ad67fe9989ae5
Wesley Leung <Wes.k.Leung@gmail.com>
2013-05-05 16:32:52 -0700

    fixed server crash due to missing messageType object

--- routes/sendgrid.js
+++ routes/sendgrid.js
@@ -1,14 +1,18 @@
 /* --------- API Function ------------ */
 /*API call: /api/sendgrid/alert
 [messageType] Message type - single word "invite" or "alert"
-[initiator] Person initiating the conference call or an alert that he or she is running late 
+[initiator] Email of person initiating the conference call or an alert that he or she is running late 
 [attendees] Unique identifier for a conference call. //e.g. ["mjgrogers@gmail.com", "bossmobilewunderkinds@lists.stanford.edu", .....]
 [phoneNumber] Phone number of conference call
 [conferenceCode] Conference code for call
 [event] event json object  {'name': 'event name', 'start':{'datetime': date time object, 'timezone':'Pacific'}  }
 */
+
 var Email = require('sendgrid').Email;
 var SendGrid = require('sendgrid').SendGrid;
+
+//Update this upon app store approval.
+var downloadURL = "http://itunes.com/apps/callinapp"  
 
 exports.emailAlert = function(req, res) {
 	var user, key; 
@@ -30,7 +34,7 @@ exports.emailAlert = function(req, res) {
     var conferenceAttendees = req.body.attendees;
     var conferencePhoneNumber = req.body.phoneNumber;
     var conferenceCode = req.body.conferenceCode;
-    var event = req.body.event;
+    var event = req.body.event ? req.body.event: "";
 
     var startTime = stringifyTimeObject(event);
  
@@ -38,21 +42,24 @@ exports.emailAlert = function(req, res) {
     var sender, msgSubject, body;
     if(messageType == 'invite') {
         sender = 'Invite@CallInapp.com';
-        msgSubject = "Call invite from: " + initiator + " for " + event.name " at " + startTime;
-        body = "You're receiving this email because " + initiator + " has initiated a conference call with CallIn, and would like you to join!";
+        msgSubject = "Phone cal: " + initiator + " for " + event.name + " at " + startTime;
+        body = initiator + " has invited you to join a conference call through CallinApp. To join, download Callin at: " + downloadURL + ".\n\n"  
+            + "You may also dial-in: " + conferencePhoneNumber + ".\n" 
+            + "With code: " + conferenceCode + ".\n";
+
     } else if (messageType == 'alert') {
         sender = 'Alert@CallInapp.com';
-        msgSubject = initiator + " has sent you an alert regarding your upcoming conference call";
+        msgSubject = initiator + " is running late for your call: " + event.name + "at " + conferenceCode; 
         body = "Sometimes life throws you curveballs, and it's how you respond that defines you. That's why you're receiving this email: to let you know that " + initiator + " is running late and will be joining the conference call as soon as possible.";
     }
     var email = new Email({
-        // to: conferenceAttendees, 
         from: sender,
+        replyto: initiator,
         subject: msgSubject,
         text: body
     });
     email.addTo(conferenceAttendees);
-
+    console.log(email);
     sendgrid.send(email, function(success, message) {
         if(!success) {
             var response = {"meta": {"code": 400},
