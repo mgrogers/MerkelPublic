4e596f623b6843e98c0361d3cb7997aa1e0b6d7d
Wesley Leung <Wes.k.Leung@gmail.com>
2013-05-05 15:46:16 -0700

    added date time support

--- routes/sendgrid.js
+++ routes/sendgrid.js
@@ -1,15 +1,17 @@
 /* --------- API Function ------------ */
 /*API call: /api/sendgrid/alert
-[msgType] Message type - single word "invite" or "alert"
+[messageType] Message type - single word "invite" or "alert"
 [initiator] Person initiating the conference call or an alert that he or she is running late 
-[attendees] Unique identifier for a conference call 
+[attendees] Unique identifier for a conference call. //e.g. ["mjgrogers@gmail.com", "bossmobilewunderkinds@lists.stanford.edu", .....]
+[phoneNumber] Phone number of conference call
+[conferenceCode] Conference code for call
+[event] event json object  {'name': 'event name', 'start':{'datetime': date time object, 'timezone':'Pacific'}  }
 */
 var Email = require('sendgrid').Email;
 var SendGrid = require('sendgrid').SendGrid;
 
 exports.emailAlert = function(req, res) {
 	var user, key; 
-
     if(!process.env.SENDGRID_USERNAME) {
 		user = "app12018585@heroku.com";
 	} else {
@@ -23,42 +25,49 @@ exports.emailAlert = function(req, res) {
     var sendgrid = new SendGrid(user, key);
 
 
-    var messageType = req.body.msgType;
+    var messageType = req.body.messageType;
     var initiator = req.body.initiator;
     var conferenceAttendees = req.body.attendees;
+    var conferencePhoneNumber = req.body.phoneNumber;
+    var conferenceCode = req.body.conferenceCode;
+    var event = req.body.event;
+
+    var startTime = stringifyTimeObject(event);
+ 
 
     var sender, msgSubject, body;
-
-
-
     if(messageType == 'invite') {
         sender = 'Invite@CallInapp.com';
-        msgSubject = initiator + " has invited you to a conference call with CallIn!";
+        msgSubject = "Call invite from: " + initiator + " for " + event.name " at " + startTime;
         body = "You're receiving this email because " + initiator + " has initiated a conference call with CallIn, and would like you to join!";
-    } else {
+    } else if (messageType == 'alert') {
         sender = 'Alert@CallInapp.com';
         msgSubject = initiator + " has sent you an alert regarding your upcoming conference call";
-        body = "Sometimes life throws you curveballs, and it's how you respond that defines you. That's why you're receiving this email: to let you know that " + initiator + " is running lateand will be joining the conference call as soon as possible.";
-     }
+        body = "Sometimes life throws you curveballs, and it's how you respond that defines you. That's why you're receiving this email: to let you know that " + initiator + " is running late and will be joining the conference call as soon as possible.";
+    }
     var email = new Email({
-        // to: conferenceAttendees, //e.g. ["mjgrogers@gmail.com", "bossmobilewunderkinds@lists.stanford.edu", .....]
+        // to: conferenceAttendees, 
         from: sender,
         subject: msgSubject,
         text: body
     });
     email.addTo(conferenceAttendees);
-    
+
     sendgrid.send(email, function(success, message) {
         if(!success) {
-            console.log("failed" + message);
             var response = {"meta": {"code": 400},
-      	                 "message": "Invitation delivery failed"};
+      	                 "message": "Invitation delivery failed. " + message};
             res.send(400, response);
 		} else {
-            console.log("succeed" + success);
             var response = {"meta": {"code": 200},
-                         "message": "Invite delivered"};    
+                         "message": "Invite delivered to :" + conferenceAttendees};    
             res.send(response);
         }
     });
+ }
+
+function stringifyTimeObject(timeObject) {
+    var date = new Date(timeObject.dateTime);
+    //need to write additional logic here to format the readable string according to recipient timezone
+    return date.toString();
  }
