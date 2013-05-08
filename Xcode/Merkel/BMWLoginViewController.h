0b8817322588b9de4cfe5b71dd9781ff23cbcf4c
Tim Shi <timgshi@gmail.com>
2013-05-08 03:24:22 -0700

    starting login view controller

---
+++ Xcode/Merkel/BMWLoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  BMWLoginViewController.h
+//  Merkel
+//
+//  Created by Tim Shi on 5/8/13.
+//  Copyright (c) 2013 BossMobileWunderkinds. All rights reserved.
+//
+
+#import "GAITrackedViewController.h"
+
+@interface BMWLoginViewController : GAITrackedViewController
+
+@end
---
+++ Xcode/Merkel/BMWLoginViewController.m
@@ -0,0 +1,39 @@
+//
+//  BMWLoginViewController.m
+//  Merkel
+//
+//  Created by Tim Shi on 5/8/13.
+//  Copyright (c) 2013 BossMobileWunderkinds. All rights reserved.
+//
+
+#import "BMWLoginViewController.h"
+
+@interface BMWLoginViewController ()
+@property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+
+@end
+
+@implementation BMWLoginViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
