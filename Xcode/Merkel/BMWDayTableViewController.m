aaa1d594d70345d6b5cdaa0c0942077750222f4e
Wesley Leung <Wes.k.Leung@gmail.com>
2013-05-08 04:25:21 -0700

    client loops sms by single person

--- Xcode/Merkel/BMWDayTableViewController.m
+++ Xcode/Merkel/BMWDayTableViewController.m
@@ -273,20 +273,25 @@ static NSString * const kInviteMessageType = @"invite";
     EKEvent *event = self.calendarEvents[index][@"event"];
 
     [[BMWCalendarAccess sharedAccess] attendeesForEvent:event withCompletion:^(NSArray *attendees) {
-        NSDictionary *parameters = [NSDictionary dictionaryWithObjectsAndKeys:
-                                    event.title, @"title",
-                                    event.startDate, @"startTime",
-                                    phoneNumber, @"phoneNumber",
-                                    conferenceCode, @"conferenceCode",
-                                    attendees, @"attendees",
-                                    kAlertMessageType, @"messageType",
-                                    kTestSenderEmailAddress, @"initiator",nil];
         
-        [[BMWAPIClient sharedClient] sendSMSMessageWithParameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
-            NSLog(@"Alert success with response %@", responseObject);
-        } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
-            NSLog(@"Error sending message", [error localizedDescription]);
-        }];
+        for (int i = 0; i < [attendees count]; i++) {
+            NSString *attendeePhone = [attendees[i] objectForKey:@"phone"];
+    
+            NSDictionary *parameters = [NSDictionary dictionaryWithObjectsAndKeys:
+                                        event.title, @"title",
+                                        event.startDate, @"startTime",
+                                        phoneNumber, @"phoneNumber",
+                                        conferenceCode, @"conferenceCode",
+                                        attendeePhone, @"toPhoneNumber",
+                                        kAlertMessageType, @"messageType",
+                                        kTestSenderEmailAddress, @"initiator",nil];
+            
+            [[BMWAPIClient sharedClient] sendSMSMessageWithParameters:parameters success:^(AFHTTPRequestOperation *operation, id responseObject) {
+                NSLog(@"Alert success with response %@", responseObject);
+            } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
+                NSLog(@"Error sending message", [error localizedDescription]);
+            }];
+        }
     }];
     
     [self.tableView endUpdates];
