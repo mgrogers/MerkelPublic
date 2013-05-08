76a1f08f4214a789049f9416f9725d16b0796a5c
Tim Shi <timgshi@gmail.com>
2013-05-08 03:58:45 -0700

    keyboard moves for second input field

--- Xcode/Merkel/BMWLoginViewController.m
+++ Xcode/Merkel/BMWLoginViewController.m
@@ -8,8 +8,11 @@
 
 #import "BMWLoginViewController.h"
 
-@interface BMWLoginViewController ()
+@interface BMWLoginViewController () <UITextFieldDelegate>
 @property (strong, nonatomic) IBOutlet UILabel *titleLabel;
+@property (strong, nonatomic) IBOutlet UITextField *phoneNumberField;
+@property (strong, nonatomic) IBOutlet UILabel *secondFieldLabel;
+@property (strong, nonatomic) IBOutlet UITextField *secondField;
 
 @end
 
@@ -27,13 +30,56 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view.
+	self.view.backgroundColor = [UIColor bmwDarkBlueColor];
+    UITapGestureRecognizer *tapGR = [[UITapGestureRecognizer alloc] initWithTarget:self action:@selector(screenTapped:)];
+    [self.view addGestureRecognizer:tapGR];
 }
 
-- (void)didReceiveMemoryWarning
-{
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (void)viewWillAppear:(BOOL)animated {
+    [super viewWillAppear:animated];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillShow:) name:UIKeyboardWillShowNotification object:nil];
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(keyboardWillHide:) name:UIKeyboardWillHideNotification object:nil];
+}
+
+- (void)viewWillDisappear:(BOOL)animated {
+    [super viewWillDisappear:animated];
+    [[NSNotificationCenter defaultCenter] removeObserver:self];
+}
+
+- (void)screenTapped:(UITapGestureRecognizer *)tapGR {
+    [self.phoneNumberField resignFirstResponder];
+    [self.secondField resignFirstResponder];
+}
+
+- (void)keyboardWillShow:(NSNotification *)notification {
+    if ([self.secondField isFirstResponder]) {
+        CGRect frame = self.view.frame;
+        CGRect keyboard = [[notification.userInfo objectForKey:UIKeyboardFrameEndUserInfoKey] CGRectValue];
+        frame.origin.y -= keyboard.size.height;
+        frame.origin.y += 20;
+        [UIView animateWithDuration:[[notification.userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue] animations:^{
+            self.view.frame = frame;
+        }];
+    }
+}
+
+- (void)keyboardWillHide:(NSNotification *)notification {
+    CGRect frame = self.view.frame;
+    frame.origin.y = 0;
+    [UIView animateWithDuration:[[notification.userInfo objectForKey:UIKeyboardAnimationDurationUserInfoKey] doubleValue] animations:^{
+        self.view.frame = frame;
+    }];
+}
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    return YES;
+}
+
+- (void)textFieldDidEndEditing:(UITextField *)textField {
+}
+
+- (BOOL)textField:(UITextField *)textField shouldChangeCharactersInRange:(NSRange)range replacementString:(NSString *)string {
+    return YES;
 }
 
 @end
