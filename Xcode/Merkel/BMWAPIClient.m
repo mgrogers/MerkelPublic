f7355ed7a405001e46903cf824042c1b4ba8b6c8
Wesley Leung <Wes.k.Leung@gmail.com>
2013-05-20 03:29:55 -0700

    filter duplicate events

--- Xcode/Merkel/BMWAPIClient.m
+++ Xcode/Merkel/BMWAPIClient.m
@@ -73,6 +73,7 @@ static NSString * const kBMWAPIClientBaseURLString = @"http://api.callinapp.com/
                                 event.notes, @"description",
                                 event.startDate, @"start",
                                 event.creationDate, @"creationDate",
+                                [PFUser currentUser].email, @"initiator",
                                 attendeesArray, @"attendees", nil];
     [self postPath:kBMWNewConferencePath
         parameters:parameters
