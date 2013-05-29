661cbbc0d2d671cd4add5e325c400c42065c7eb8
Tim Shi <timgshi@gmail.com>
2013-05-29 02:26:26 -0700

    added pony debugger through pods

--- Xcode/Pods/Pods-environment.h
+++ Xcode/Pods/Pods-environment.h
@@ -37,11 +37,23 @@
 // Please contact the author.
 // Version: 1.0b2.
 
+// PonyDebugger
+#define COCOAPODS_POD_AVAILABLE_PonyDebugger
+#define COCOAPODS_VERSION_MAJOR_PonyDebugger 0
+#define COCOAPODS_VERSION_MINOR_PonyDebugger 3
+#define COCOAPODS_VERSION_PATCH_PonyDebugger 0
+
 // Reachability
 #define COCOAPODS_POD_AVAILABLE_Reachability
 #define COCOAPODS_VERSION_MAJOR_Reachability 3
 #define COCOAPODS_VERSION_MINOR_Reachability 1
 #define COCOAPODS_VERSION_PATCH_Reachability 0
+
+// SocketRocket
+#define COCOAPODS_POD_AVAILABLE_SocketRocket
+#define COCOAPODS_VERSION_MAJOR_SocketRocket 0
+#define COCOAPODS_VERSION_MINOR_SocketRocket 2
+#define COCOAPODS_VERSION_PATCH_SocketRocket 0
 
 // TestFlightSDK
 #define COCOAPODS_POD_AVAILABLE_TestFlightSDK
